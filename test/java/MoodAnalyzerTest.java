92d8e92376153119e752b0d6ff15c51eec808b04
PUJAPURVI <nerkarpuja@gmail.com>
2023-01-02 11:37:03 +0530

    Added: Ability to analyse and respond mood

---
+++ test/java/MoodAnalyzerTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMood_WhenHappy_ShouldReturnHappy() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        String mood = moodAnalyser.analyzeMood("HAPPY");
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenMood_WhenSAD_ShouldReturnSAD() {
+        MoodAnalyzer moodAnalyser = new MoodAnalyzer();
+        String mood = moodAnalyser.analyzeMood("SAD");
+        Assert.assertEquals("SAD", mood);
+    }
+}
+
+
+
